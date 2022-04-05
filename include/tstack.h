36cb5c2f959e853a6fd5f0254533e0f4d3371fef
vektor0 <99178713+vektor0@users.noreply.github.com>
2022-04-05 12:41:59 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
+//zxcvbnm
 template<typename T, int size>
 class TStack {
-
+//zxcvbnm
 };
 #endif  // INCLUDE_TSTACK_H_
